e80dccfb7fd6ab9c4b4da7ad3524c7267481aab1
LimeEng <35342122+LimeEng@users.noreply.github.com>
2018-03-03 21:10:24 +0100

    Added a private constructor (#26)
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/main/java/alphabet/AlphabetFactory.java
+++ src/main/java/alphabet/AlphabetFactory.java
@@ -6,6 +6,9 @@ import java.util.stream.Stream;
 
 public class AlphabetFactory {
 
+	// To prevent instantiation
+	private AlphabetFactory() {}
+
 	public static Alphabet of(String characters) {
 		return getAlphabet(characters);
 	}
@@ -13,13 +16,14 @@ public class AlphabetFactory {
 	public static Alphabet of(Collection<Integer> codePoints) {
 		return getAlphabet(codePoints);
 	}
-	
+
 	private static Alphabet getAlphabet(Stream<Integer> codePoints) {
 		return new DefinedAlphabet(distinct(codePoints).collect(Collectors.toList()));
 	}
 
 	private static Alphabet getAlphabet(String text) {
-		return getAlphabet(text.codePoints().boxed());
+		return getAlphabet(text.codePoints()
+				.boxed());
 	}
 
 	private static Alphabet getAlphabet(Collection<Integer> codePoints) {
